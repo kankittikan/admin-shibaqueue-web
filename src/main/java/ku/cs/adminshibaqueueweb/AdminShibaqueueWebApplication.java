dbfd915e65ad4bc10332b01b5d2e1bf4a0e1fb23
kankittikan <nongkan195@gmail.com>
2023-10-03 20:05:27 +0700

    init spring

---
+++ src/main/java/ku/cs/adminshibaqueueweb/AdminShibaqueueWebApplication.java
@@ -0,0 +1,13 @@
+package ku.cs.adminshibaqueueweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminShibaqueueWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminShibaqueueWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ku/cs/adminshibaqueueweb/AdminShibaqueueWebApplicationTests.java
@@ -0,0 +1,13 @@
+package ku.cs.adminshibaqueueweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminShibaqueueWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
